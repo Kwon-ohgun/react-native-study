61dd6c8042f9d48f98f5a613022e8c2e7eb65165
ohgun <dhrjs0128@naver.com>
2021-12-05 18:52:24 +0900

    [#2] 다른 Screen으로 Navigator 이동

---
+++ screens/Tab1Detail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Tab1Detail() {
+  return (
+    <View>
+      <Text>Tab1Detail</Text>
+    </View>
+  );
+}
