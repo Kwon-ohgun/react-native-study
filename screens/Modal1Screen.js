8938be6bb4b4e7e749bfca55bbbe4dc8ce8b9eda
ohgun <dhrjs0128@naver.com>
2021-12-05 19:58:15 +0900

    [#6] 모달창 헤더 X아이콘 추가

--- screens/Modal1Screen.js
+++ screens/Modal1Screen.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View } from 'react-native';
 
 export default function Modal1Screen({ navigation }) {
   return (
-    <View>
-      <Button
-        title="Close Modal1"
-        onPress={() => navigation.goBack()}
-      />
-    </View>
+    <View></View>
   );
 }
