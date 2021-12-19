0730297eda3cd0faece8a6e29f1e5b82516180cd
ohgun <dhrjs0128@naver.com>
2021-12-19 16:48:30 +0900

    [#12] stylesheet 연결하기

--- screens/Tab4Screen.js
+++ screens/Tab4Screen.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  wrap: {
+    flexDirection: 'row'
+  },
+  nav: {
+    // flex: 0,  // `flex: 0`은 web에서 영역이 할당 안 될 수 있으므로 주석 처리 한다.
+  },
+  section: {
+    flex: 1
+  },
+  header: {
+    flexDirection: 'row'
+  },
+  home: {
+    flex: 1
+  },
+  contents: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export default function Tab4Screen() {
   return (
-    <View name="wrap" style={{flexDirection: 'row'}}>
-      <View name="nav" style={{flex: 0}}>
+    <View name="wrap" style={styles.wrap}>
+      <View name="nav">
         <Text>⛪</Text>
         <Text>🎡</Text>
         <Text>🎠</Text>
@@ -12,12 +36,12 @@ export default function Tab4Screen() {
         <Text>📷</Text>
         <Text>📼</Text>
       </View>
-      <View name="section" style={{flex: 1}}>
-        <View name="header" style={{flexDirection: 'row'}}>
-          <Text name="home" style={{flex: 1}}>⛪</Text>
-          <Text style={{flex: 1}}>🤖</Text>
+      <View name="section" style={styles.section}>
+        <View name="header" style={styles.header}>
+          <Text name="home" style={styles.home}>⛪</Text>
+          <Text>🤖</Text>
         </View>
-        <View name="contents" style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View name="contents" style={styles.contents}>
           <Text>Home</Text>
         </View>
       </View>
