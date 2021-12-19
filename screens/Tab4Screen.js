090c5d8e0d379c94dc57fffd2fc3be1b50f92c5b
ohgun <dhrjs0128@naver.com>
2021-12-19 16:37:16 +0900

    [#12] flex 사용하여 레이아웃 잡기

--- screens/Tab4Screen.js
+++ screens/Tab4Screen.js
@@ -3,8 +3,24 @@ import { Text, View } from 'react-native';
 
 export default function Tab4Screen() {
   return (
-    <View>
-      <Text>스크린4</Text>
+    <View name="wrap" style={{flexDirection: 'row'}}>
+      <View name="nav" style={{flex: 0}}>
+        <Text>⛪</Text>
+        <Text>🎡</Text>
+        <Text>🎠</Text>
+        <Text>🎮</Text>
+        <Text>📷</Text>
+        <Text>📼</Text>
+      </View>
+      <View name="section" style={{flex: 1}}>
+        <View name="header" style={{flexDirection: 'row'}}>
+          <Text name="home" style={{flex: 1}}>⛪</Text>
+          <Text style={{flex: 1}}>🤖</Text>
+        </View>
+        <View name="contents" style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Home</Text>
+        </View>
+      </View>
     </View>
   );
 }
