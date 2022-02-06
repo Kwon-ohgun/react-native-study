66e7e9ade67f57d8de9ff3c6eab7c00c634622ec
ohgun <dhrjs0128@naver.com>
2022-02-06 16:34:07 +0900

    [#19] axios members read

--- stores/MembersStore.js
+++ stores/MembersStore.js
@@ -27,14 +27,12 @@ export default class MembersStore {
     });
   }
   membersRead() {
-    this.members = [{
-      name: '홍길동',
-      age: 20
-    }, {
-      name: '춘향이',
-      age: 16
-    }];
-    console.log('Done membersRead', this.members);
+    axios.get('http://192.168.152.106:3100/api/v1/members').then((response) => {
+      console.log('Done membersRead', response);
+      this.members = response.data.members;
+    }).catch((error) => {
+      axiosError(error);
+    });
   }
   membersDelete(index) {
     this.members.splice(index, 1);
